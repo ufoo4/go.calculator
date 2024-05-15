c64d16c9f8e7715a3b314d23e5f7f4052945446a
chisa_doe <chisa.doe@gmail.com>
2024-05-15 10:13:44 +0700

    read complete

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	var input int
-	fmt.Scanf("Что необходимо посчитать?", &input)
-	fmt.Println(input)
+	fmt.Println("Введите вычисляемое выражение:")
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Ошибка при вводе:", err)
+		return
+	}
+
+	reg := regexp.MustCompile(`[+\-*/]`)
+	parts := reg.Split(text, -1)
+	for i, v := range parts {
+		parts[i] = strings.TrimSpace(v)
+	}
+
+	fmt.Println(parts)
 }
